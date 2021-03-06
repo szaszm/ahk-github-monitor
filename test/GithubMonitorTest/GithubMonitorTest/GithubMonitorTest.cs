950c454f7652f68e2ddebcbddebea572ef0b089e
Marton Szasz <szaszm01@gmail.com>
2021-03-06 13:47:21 +0100

    test: fully cover GithubMonitorFunction and PayloadValidator

--- test/GithubMonitorTest/GithubMonitorTest/GithubMonitorTest.cs
+++ test/GithubMonitorTest/GithubMonitorTest/GithubMonitorTest.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Ahk.GitHub.Monitor;
 using Ahk.GitHub.Monitor.Services;
@@ -26,16 +32,45 @@ namespace GithubMonitorTest
         private const string PrivateKey = "privatekey";
         private const string WebhookSecret = "webhooksecret";
 
-        private ILogger logger = NullLoggerFactory.Instance.CreateLogger("null logger");
+        private readonly ILogger logger = NullLoggerFactory.Instance.CreateLogger("null logger");
 
         private IOptions<GitHubMonitorConfig> GetConfig(string appId = AppId, string privateKey = PrivateKey, string webhookSecret = WebhookSecret)
         {
-            return Options.Create<GitHubMonitorConfig>(new GitHubMonitorConfig()
+            return Options.Create(new GitHubMonitorConfig
             {
                 GitHubAppId = appId,
                 GitHubAppPrivateKey = privateKey,
                 GitHubWebhookSecret = webhookSecret
             });
+        }
+
+        private string SignPayload(byte[] payload)
+        {
+            var hmacsha1 = new HMACSHA1(Encoding.ASCII.GetBytes(WebhookSecret));
+            var binarySignature = hmacsha1.ComputeHash(payload);
+            var hexString = BitConverter.ToString(binarySignature).Replace("-", string.Empty).ToLower();
+            return "sha1=" + hexString;
+        }
+
+        private static void ParseRequestFile(string content, HttpRequest request)
+        {
+            content = content.Replace("\r\n", "\n");
+            var headerSeparatorIndex = content.IndexOf("\n\n", StringComparison.Ordinal);
+            var headers = content.Substring(0, headerSeparatorIndex)
+                .Split("\n")
+                .Select(line => line.Split(':').Select(s => s.Trim()))
+                .ToDictionary(line => line.First(), line => line.Last());
+            foreach (var (key, value) in headers)
+            {
+                request.Headers[key] = value;
+            }
+
+            var body = content.Substring(headerSeparatorIndex + "\n\n".Length);
+            request.ContentLength = body.Length;
+            request.Body = new MemoryStream(Encoding.UTF8.GetBytes(body));
+
+            request.Method = headers["Request method"];
+            request.ContentType = headers["content-type"];
         }
 
         [TestMethod]
@@ -60,14 +95,77 @@ namespace GithubMonitorTest
             var functionPrivateKeyNull = new GitHubMonitorFunction(dispatcher, GetConfig(AppId, null));
             var functionWebhookSecretNull = new GitHubMonitorFunction(dispatcher, GetConfig(AppId, PrivateKey, null));
 
-            ObjectResult responseAppidNull = (ObjectResult) await functionAppIdNull.Run(request, logger);
-            ObjectResult responsePrivateKeyNull = (ObjectResult) await functionPrivateKeyNull.Run(request, logger);
-            ObjectResult responseWebhookSecretNull =
-                (ObjectResult) await functionWebhookSecretNull.Run(request, logger);
+            var responseAppidNull = (ObjectResult) await functionAppIdNull.Run(request, logger);
+            var responsePrivateKeyNull = (ObjectResult) await functionPrivateKeyNull.Run(request, logger);
+            var responseWebhookSecretNull = (ObjectResult) await functionWebhookSecretNull.Run(request, logger);
 
             Assert.AreEqual(StatusCodes.Status500InternalServerError, responseAppidNull?.StatusCode);
             Assert.AreEqual(StatusCodes.Status500InternalServerError, responsePrivateKeyNull?.StatusCode);
             Assert.AreEqual(StatusCodes.Status500InternalServerError, responseWebhookSecretNull?.StatusCode);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"resources/invalid_signature_rejected.txt")]
+        public async Task InvalidSignatureRejected()
+        {
+            var context = new DefaultHttpContext();
+            var request = context.Request;
+            var function = new GitHubMonitorFunction(new NullEventDispatchService(), GetConfig());
+            var fileContent =
+                await File.ReadAllTextAsync(Path.Combine(Directory.GetCurrentDirectory(),
+                    @"invalid_signature_rejected.txt"));
+            ParseRequestFile(fileContent, request);
+
+            var response = (ObjectResult) await function.Run(request, logger);
+
+            Assert.AreEqual(StatusCodes.Status400BadRequest, response?.StatusCode);
+            var errorProperty = response?.Value.GetType().GetProperty("error");
+            Assert.AreEqual("Payload signature not valid", errorProperty?.GetValue(response.Value));
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"resources/valid_signature_accepted.txt")]
+        public async Task ValidSignatureAccepted()
+        {
+            var context = new DefaultHttpContext();
+            var request = context.Request;
+            var function = new GitHubMonitorFunction(new NullEventDispatchService(), GetConfig());
+            var fileContent =
+                await File.ReadAllTextAsync(Path.Combine(Directory.GetCurrentDirectory(),
+                    @"valid_signature_accepted.txt"));
+            ParseRequestFile(fileContent, request);
+            var contentBytes = new byte[request.Body.Length];
+            await request.Body.ReadAsync(contentBytes, 0, contentBytes.Length);
+            request.Body.Seek(0, SeekOrigin.Begin);
+
+            var response = (ObjectResult)await function.Run(request, logger);
+
+            Assert.AreEqual("{}", Encoding.UTF8.GetString(contentBytes));
+            Assert.AreEqual(StatusCodes.Status200OK, response?.StatusCode);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"resources/valid_signature_accepted.txt")]
+        public async Task ExceptionsAreLogged()
+        {
+            var context = new DefaultHttpContext();
+            var request = context.Request;
+            var throwingDispatcher = new Mock<IEventDispatchService>();
+            throwingDispatcher.Setup(x => x.Process(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<WebhookResult>()))
+                .Throws(new Exception("test exception"));
+            var function = new GitHubMonitorFunction(throwingDispatcher.Object, GetConfig());
+            var fileContent =
+                await File.ReadAllTextAsync(Path.Combine(Directory.GetCurrentDirectory(),
+                    @"valid_signature_accepted.txt"));
+            ParseRequestFile(fileContent, request);
+
+            var response = (ObjectResult)await function.Run(request, logger);
+
+            Assert.IsNotNull(response);
+            var messagesField = typeof(WebhookResult).GetField("Messages");
+            var messages = messagesField?.GetValue(response.Value) as List<string>;
+            Assert.IsNotNull(messages);
+            Assert.IsTrue(messages.First().Contains("test exception"));
         }
     }
 }
--- test/GithubMonitorTest/GithubMonitorTest/PayloadValidatorTest.cs
+++ test/GithubMonitorTest/GithubMonitorTest/PayloadValidatorTest.cs
@@ -8,27 +8,34 @@ namespace GithubMonitorTest
     public class PayloadValidatorTest
     {
         private const string Key = "testSecret";
+        private static readonly byte[] testString = Encoding.ASCII.GetBytes("testString");
 
         [TestMethod]
         public void ValidSecret()
         {
-            Assert.IsTrue(PayloadValidator.IsSignatureValid(Encoding.ASCII.GetBytes("testString"),
+            Assert.IsTrue(PayloadValidator.IsSignatureValid(testString,
                 "sha1=17776af870091664d05e1a90e22f3cfb181b5411", Key));
         }
 
         [TestMethod]
         public void InvalidSecretFails()
         {
-            Assert.IsFalse(PayloadValidator.IsSignatureValid(Encoding.ASCII.GetBytes("testString"),
+            Assert.IsFalse(PayloadValidator.IsSignatureValid(testString,
                 "sha1=17776af870091664d05e1a90e22f3cfb181b5411", "InvalidKey"));
         }
 
         [TestMethod]
         public void InvalidSignatureFails()
         {
-            Assert.IsFalse(PayloadValidator.IsSignatureValid(Encoding.ASCII.GetBytes("testString"),
+            Assert.IsFalse(PayloadValidator.IsSignatureValid(testString,
                 "sha1=ab776af870091664d05e1a90e22f3cfb181b54cd", Key));
         }
 
+        [TestMethod]
+        public void EmptySignatureFails()
+        {
+            Assert.IsFalse(PayloadValidator.IsSignatureValid(testString,
+                string.Empty, Key));
+        }
     }
 }
